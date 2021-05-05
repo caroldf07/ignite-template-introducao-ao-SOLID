71870bd280c27439e45c9056d7786faa9457d8ab
caroldf07 <carolina.df@outlook.com>
2021-05-05 18:27:05 -0300

    refatoração e finalização do desafio

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import swaggerUi from "swagger-ui-express";
-import swaggerFile from "./swagger.json";
 
 import { app } from ".";
+import swaggerFile from "./swagger.json";
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
